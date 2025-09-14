7a29a731863b20d151c992e40bcde0c4dd4970dc
Mtende Kuyokwa <mtendekuyokwa19@gmail.com>
2025-09-14 11:29:03 +0200

    compilation errors

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import { Analytics } from "@vercel/analytics/next";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { Maven_Pro, Rowdies, Pacifico } from "next/font/google";
 import "./globals.css";
-import { Footer, Navbar, ToTop } from "@/app/page";
+import { Footer, Navbar, ToTop } from "@/app/page.jsx";
 const inter = Pacifico({
   subsets: ["latin"],
   display: "swap",
